8c135eac9cdcbf9c534f2b65b4a1a932b19b53e2
Collin Cappelle <collin.cappelle@gmail.com>
2018-10-31 23:31:47 -0400

    Instance rendering

--- modeling/js/odes.js
+++ modeling/js/odes.js
@@ -1,4 +1,53 @@
 
+var ARROW_VERTEX_SHADER = `
+    precision highp float;
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+
+    attribute vec3 position;
+    attribute vec3 offset;
+    attribute vec3 direction;
+    // attribute vec3 scale;
+    // attribute vec3 color;
+    const vec3 scale = vec3( 0.1, 0.1, 0.1 );
+    varying vec4 vPosition;
+    
+
+    void main(){
+        vec3 normalizedDirection = normalize( vec3( direction.x, -direction.y, direction.z ) );
+        float theta = atan( normalizedDirection.y, normalizedDirection.x );
+        // theta = 3.14159 / 2.0;
+        mat4 S = mat4( scale.x, 0.0, 0.0, 0.0,
+                       0.0, scale.y, 0.0, 0.0,
+                       0.0, 0.0, scale.z, 0.0,
+                       0.0, 0.0, 0.0,     1.0 );
+
+        mat4 R = mat4(  cos( theta ), -sin( theta ), 0.0, 0.0,
+                        sin( theta ),  cos( theta ), 0.0, 0.0,
+                        0.0,          0.0          , 1.0, 0.0,
+                        0.0,          0.0          , 0.0, 1.0 );
+
+        mat4 T = mat4( 1.0, 0.0, 0.0, offset.x,
+                       0.0, 1.0, 0.0, offset.y,
+                       0.0, 0.0, 1.0, offset.z,
+                       0.0, 0.0, 0.0, 1.0 );
+
+        vPosition = vec4( offset, 0.0 ) + R * S * vec4( position, 1.0 );//  T * R * S * vec4( position, 1.0 );
+        // vColor = vec4( vColor, 1.0 );
+        gl_Position = projectionMatrix * modelViewMatrix * vPosition;
+    }
+`
+
+var ARROW_FRAGMENT_SHADER = `
+    precision highp float;
+    // varying vColor;
+    varying vec4 vPosition;
+
+    void main(){
+        gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );
+    }
+`
+
 class ODESolvers{
 
     static eulerStep( f, t, h, yInits ){
@@ -123,7 +172,46 @@ class ODEDemo extends SuperModel{
                     );
             }
         }
+        var arrowGeom = this.getArrowGeometry();
+        this.arrowInstance = new THREE.InstancedBufferGeometry();
 
+        this.arrowInstance.index = arrowGeom.index;
+        this.arrowInstance.attributes.position = arrowGeom.attributes.position;
+
+        this.N_INSTANCES = 21 * 21 * 3;
+        this.arrowPositions = new Float32Array( this.N_INSTANCES );
+        this.slopeVectors = new Float32Array( this.N_INSTANCES );
+
+        var count = 0;
+        for ( var x = -5; x <= 5; x += 0.5 ){
+            for ( var y = -5; y <= 5; y += 0.5 ){
+                this.slopeVectors[count] = Math.random() * 2 - 1.0;
+                this.arrowPositions[ count++ ] = x;
+
+                this.slopeVectors[count] = Math.random() * 2 - 1.0;
+                this.arrowPositions[ count++ ] = y;
+
+                this.slopeVectors[count] = Math.random();
+                this.arrowPositions[ count++ ] = 0;
+            }
+        }
+
+        this.arrowInstance.addAttribute( 'offset', new THREE.InstancedBufferAttribute( this.arrowPositions, 3 ) );
+        this.arrowInstance.addAttribute( 'direction', new THREE.InstancedBufferAttribute( this.slopeVectors, 3 ) );
+
+        var instancedMaterial = new THREE.RawShaderMaterial( {
+            vertexShader: ARROW_VERTEX_SHADER,
+            fragmentShader: ARROW_FRAGMENT_SHADER,
+        });
+
+
+        this.arrowInstanceMesh = new THREE.Mesh( this.arrowInstance, instancedMaterial );
+        // this.arrowInstanceMesh.scale.set( 1, 0.1, 0.1 );
+        this.addMesh( scene, this.arrowInstanceMesh );
+        var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+        var mesh = new THREE.Mesh( arrowGeom, material );
+
+        this.addMesh( scene, mesh );
         this.lineMeshes = [];
 
     }
@@ -133,6 +221,20 @@ class ODEDemo extends SuperModel{
             this.udpateModel( scene );
             this.modelShouldUpdate = false;
         }
+    }
+
+    getArrowGeometry( headpercent=0.1, n = 10 ){
+
+        var vertices = [ 0.0, 0.0, 0.0,
+                     -2.0, 1.0, 0.0,
+                     -2.0, -1.0, 0.0 ];
+
+        var vertsArray = new Float32Array( vertices );
+        var bufferGeometry = new THREE.BufferGeometry();
+
+        bufferGeometry.addAttribute( 'position', new THREE.BufferAttribute( vertsArray, 3) );
+
+        return bufferGeometry;
     }
         
     udpateModel( scene ){
@@ -145,6 +247,7 @@ class ODEDemo extends SuperModel{
         var slopeLengths = [];
 
         var index = 0;
+        var count = 0;
         for ( var t = -5; t <= 5; t += 0.5 ){
             var yStart = -5
             var yEnd = 5
@@ -156,6 +259,11 @@ class ODEDemo extends SuperModel{
                 var dir = new THREE.Vector3( 1, sample, 0 );
                 var length = dir.length();
                 slopeLengths.push( length );
+
+                var otherDir = new THREE.Vector3( sample, 1, 0 );
+                this.arrowInstance.attributes.direction[ count++ ] = dir.x;
+                this.arrowInstance.attributes.direction[ count++ ] = dir.y;
+                this.arrowInstance.attributes.direction[ count++ ] = dir.z;
 
                 dir.normalize();
 
@@ -171,6 +279,8 @@ class ODEDemo extends SuperModel{
                 index += 1;
             }
         }
+
+        this.arrowInstance.attributes.direction.needsUpdate = true;
 
         var maxLength = Math.max( ...slopeLengths );
         maxLength += maxLength * 0.01;
