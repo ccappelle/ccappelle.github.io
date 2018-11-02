1e95a82b5551981efad31b5105e7b9d1cf6ab535
Collin Cappelle <collin.cappelle@gmail.com>
2018-11-02 10:21:29 -0400

    Euler finished

--- modeling/js/odes.js
+++ modeling/js/odes.js
@@ -3,48 +3,62 @@ var ARROW_VERTEX_SHADER = `
     precision highp float;
     uniform mat4 modelViewMatrix;
     uniform mat4 projectionMatrix;
+    uniform bool shouldNormalize;
+    uniform float scale;
+    uniform bool useColor;
+    uniform float arrowAlpha;
 
     attribute vec3 position;
     attribute vec3 offset;
     attribute vec3 direction;
-    // attribute vec3 scale;
-    // attribute vec3 color;
-    const vec3 scale = vec3( 0.1, 0.1, 0.1 );
-    varying vec4 vPosition;
-    
+    attribute vec3 color;
+
+    vec4 vPosition;
+
+    varying vec4 vColor;
 
     void main(){
-        vec3 normalizedDirection = normalize( vec3( direction.x, -direction.y, direction.z ) );
-        float theta = atan( normalizedDirection.y, normalizedDirection.x );
-        // theta = 3.14159 / 2.0;
-        mat4 S = mat4( scale.x, 0.0, 0.0, 0.0,
-                       0.0, scale.y, 0.0, 0.0,
-                       0.0, 0.0, scale.z, 0.0,
-                       0.0, 0.0, 0.0,     1.0 );
+        vPosition = vec4( position.x, position.y, position.z, 1.0 );
 
-        mat4 R = mat4(  cos( theta ), -sin( theta ), 0.0, 0.0,
-                        sin( theta ),  cos( theta ), 0.0, 0.0,
+        vec3 scaleVector = vec3( scale );
+
+        float angle = atan( direction.y, direction.x );
+        float directionLength = length( direction );
+
+
+        if ( shouldNormalize == false ){
+            scaleVector.x = scaleVector.x * directionLength;
+        }
+
+        mat4 S = mat4( scaleVector.x, 0.0, 0.0, 0.0,
+                       0.0, scaleVector.y, 0.0, 0.0,
+                       0.0, 0.0, scaleVector.z, 0.0,
+                       0.0, 0.0, 0.0,   1.0 );
+
+        mat4 R = mat4(  cos( angle ), sin( angle ), 0.0, 0.0,
+                        - sin( angle ),  cos( angle ), 0.0, 0.0,
                         0.0,          0.0          , 1.0, 0.0,
                         0.0,          0.0          , 0.0, 1.0 );
 
-        mat4 T = mat4( 1.0, 0.0, 0.0, offset.x,
-                       0.0, 1.0, 0.0, offset.y,
-                       0.0, 0.0, 1.0, offset.z,
-                       0.0, 0.0, 0.0, 1.0 );
 
-        vPosition = vec4( offset, 0.0 ) + R * S * vec4( position, 1.0 );//  T * R * S * vec4( position, 1.0 );
-        // vColor = vec4( vColor, 1.0 );
+        vPosition = vec4( offset.x, offset.y, offset.z, 0.0 ) + R * S * vPosition;
+        if ( useColor == true ){
+            vColor = vec4( color, arrowAlpha );
+        } else {
+            vColor = vec4( 0.5, 0.5, 0.5, arrowAlpha);
+        }
+        
         gl_Position = projectionMatrix * modelViewMatrix * vPosition;
     }
 `
 
 var ARROW_FRAGMENT_SHADER = `
     precision highp float;
-    // varying vColor;
-    varying vec4 vPosition;
+    varying vec4 vColor;
+    // varying vec4 vPosition;
 
     void main(){
-        gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );
+        gl_FragColor = vColor;
     }
 `
 
@@ -57,25 +71,30 @@ class ODESolvers{
 
             for ( var i = 0; i < yInits.length; i++ ){
                 var y = yInits[i];
-                y = y + f( t, y ) * h;
+                y = y + f.eval( { t: t, y: y } ) * h;
                 outYs.push( y );
             }
             return outYs;
         } else {
-            return yInits + f( t, yInits ) * h;
+            return yInits + f.eval( { t: t, y: yInits } ) * h;
         }
     }
 
     static euler( f, t0, tf, h, y0s = 0){
         // run euler integration over the time range ts
         var ys = [];
+        var ts = [];
         ys.push( y0s );
 
+        var i = 0;
+        ts.push( t0 );
         for ( var t = t0 + h; t <= tf; t += h){
             ys.push ( ODESolvers.eulerStep( f, t, h, ys[i] ) );
+            ts.push( t );
+            i ++;
         }
 
-        return ys;
+        return [ ys, ts ];
     }
 
     static rk( f, ts, y0s = 0, order = 4 ){
@@ -105,30 +124,42 @@ class ODEDemo extends SuperModel{
         this.h = 0.1;
         this.y0 = 0;
 
+        this.minColor = 0x6495ED;
+        this.maxColor = 0xd93e3e;
+
         // must go before update
         var grid = new THREE.GridHelper( 10, 10 );
         grid.rotation.x = Math.PI / 2.0;
         this.addMesh( scene, grid );
 
         this.modelShouldUpdate = true;
+        this.slopesShouldUpdate = true;
 
         this.normalizeSlopes = true;
         this.colorSlopes = true;
 
+        this.functionShouldUpdate = true;
+
         this.gui.add( this, 'yPrime' ).onFinishChange( ( e ) => { this.code = math.parse( e ).compile();
-                                                                  this.modelShouldUpdate = true } );
+                                                                  this.functionShouldUpdate = true;
+                                                                  this.slopesShouldUpdate = true; } );
         this.gui.add( this, 'tStart' )
                 .min( -5.0 ).max( 0.0 ).step( 0.01 )
-                .onChange( ( e ) => this.modelShouldUpdate = true );
+                .onChange( ( e ) => { this.functionShouldUpdate = true;
+                                     this.lineGeometry.setDrawRange( 0, Math.floor( ( 1 / this.h ) * ( this.tEnd - this.tStart) ) + 1 );
+                } );
 
         this.gui.add( this, 'tEnd' )
                 .min( 0.0 ).max( 5.0 ).step( 0.01 )
-                .onChange( ( e ) => this.modelShouldUpdate = true );
+                .onChange( ( e ) => { this.functionShouldUpdate = true;
+                                     this.lineGeometry.setDrawRange( 0, Math.floor( ( 1 / this.h ) * ( this.tEnd - this.tStart) ) + 1 );
+                } );
+
         this.gui.add( this, 'y0' )
                 .min( -5.0 )
                 .max( 5.0)
                 .step( 0.1 )
-                .onChange( ( e ) => this.modelShouldUpdate = true );
+                .onChange( ( e ) => this.functionShouldUpdate = true );
         
         this.minH = 0.005;
 
@@ -136,42 +167,78 @@ class ODEDemo extends SuperModel{
                 .min( this.minH )
                 .max( 1.0 )
                 .step( 0.001 )
-                .onChange( ( e ) => this.modelShouldUpdate = true );
+                .onChange( ( e ) => { this.functionShouldUpdate = true;
+                                     this.lineGeometry.setDrawRange( 0, Math.floor( ( 1 / this.h ) * ( this.tEnd - this.tStart) ) + 1);
+                } );
 
-        this.gui.add( this, 'normalizeSlopes' ).onChange( ( e ) => this.modelShouldUpdate = true );
-        this.gui.add( this, 'colorSlopes' ).onChange( ( e ) => this.modelShouldUpdate = true );
+
+        this.arrowAlpha = 1.0;
+
+        this.arrowFolder = this.gui.addFolder( 'Slope Field Options' );
+
+
+
+
 
         this.integratorFolder = this.gui.addFolder( 'Integrators' );
 
         this.integrators = [];
-        // add euler integrator
-        // this.eulerFolder = this.integratorFolder.addFolder( 'Forward Euler' );
-        // this.integrators.push( new ForwardEuler( this, this.eulerFolder ) );
 
-        // this.rk4Folder = this.integratorFolder.addFolder( 'RK-4' );
-        // this.integrators.push( new RK4( this, this.rk4Folder ) );
-        this.lineMaterial = new THREE.LineBasicMaterial( { color : 0x00000 } );
-        this.lineGeometetry = new THREE.Geometry();
+        this.lineMaterial = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+        this.lineGeometry = new THREE.BufferGeometry();
 
-        this.eulerLineMesh = 
-        this.arrowMeshes = [];
-        // create arrow helpers
-        for ( var i = -5; i <= 5; i += 0.5 ){
-            for ( var j = -5; j <=5; j += 0.5 ){
-                var dir = new THREE.Vector3( 1, 1, 0 );
-                dir.normalize();
-                var pos = new THREE.Vector3( i, j, 0 );
-                var arrow = new THREE.ArrowHelper( 
-                                dir, pos,
-                                0.3, 0x555555,
-                                0.2, 0.1
-                             );
-                this.addMesh( scene, arrow );
-                this.arrowMeshes.push( 
-                        arrow  
-                    );
-            }
+        this.maxLinePoints = 10 * ( 1 / this.minH );
+        this.linePointsArray = new Float32Array( this.maxLinePoints * 3 );
+
+        for ( var i = 0; i < this.maxLinePoints; i++ ){
+            this.linePointsArray[ 3 * i + 0 ] = - 5.0 + i * this.h;
+            this.linePointsArray[ 3 * i + 1 ] = 2.0;
+            this.linePointsArray[ 3 * i + 2 ] = 0.0;
         }
+        this.lineGeometry.addAttribute( 'position', new THREE.BufferAttribute( this.linePointsArray, 3 ) );
+
+
+        console.log( this.maxLinePoints / 3.0 );
+
+        this.lineMesh = new THREE.Line( this.lineGeometry, this.lineMaterial );
+        this.addMesh( scene, this.lineMesh );
+
+        this.createSlopeField();
+
+        this.lineMeshes = [];
+        this.time = 0;
+
+        this.arrowFolder.add( this, 'normalizeSlopes' )
+                        .onChange( ( value ) => this.arrowInstanceMesh.material.uniforms.shouldNormalize.value = value);
+        this.arrowFolder.add( this, 'colorSlopes' )
+                        .onChange( ( value ) => this.arrowInstanceMesh.material.uniforms.useColor.value = value);
+        this.arrowFolder.add( this, 'arrowAlpha' )
+                        .min( 0 )
+                        .max( 1.0 )
+                        .step( 0.1 )
+                        .onChange( ( value ) => { this.arrowInstanceMesh.material.uniforms.arrowAlpha.value = value 
+                            if ( value == 0 ){
+                                this.arrowInstanceMesh.material.visible = false;
+                            } else {
+                                this.arrowInstanceMesh.material.visible = true;
+                            }
+                        } );
+        this.arrowFolder.addColor( this, 'minColor' ).onChange( ( e ) => this.slopesShouldUpdate = true )
+
+        this.arrowFolder.addColor( this, 'maxColor' ).onChange( ( e ) => this.slopesShouldUpdate = true );
+        this.lineGeometry.setDrawRange( 0, Math.floor( ( 1 / this.h ) * ( this.tEnd - this.tStart) ) + 1 )
+    }
+
+    animate( scene, camera, dt ){
+
+        if ( this.slopesShouldUpdate || this.functionShouldUpdate ){
+            this.updateModel( scene, this.slopesShouldUpdate );
+            this.slopesShouldUpdate = false;
+            this.functionShouldUpdate = false;
+        }
+    }
+
+    createSlopeField(){
         var arrowGeom = this.getArrowGeometry();
         this.arrowInstance = new THREE.InstancedBufferGeometry();
 
@@ -180,54 +247,71 @@ class ODEDemo extends SuperModel{
 
         this.N_INSTANCES = 21 * 21 * 3;
         this.arrowPositions = new Float32Array( this.N_INSTANCES );
-        this.slopeVectors = new Float32Array( this.N_INSTANCES );
+        this.directionVectors = new Float32Array( this.N_INSTANCES );
+        this.colorVectors = new Float32Array( 21 * 21 * 4 );
+
 
         var count = 0;
+        var count2 = 0;
         for ( var x = -5; x <= 5; x += 0.5 ){
             for ( var y = -5; y <= 5; y += 0.5 ){
-                this.slopeVectors[count] = Math.random() * 2 - 1.0;
+                this.colorVectors[count] = 0.0;
+                this.directionVectors[count] = 1.0;
                 this.arrowPositions[ count++ ] = x;
 
-                this.slopeVectors[count] = Math.random() * 2 - 1.0;
+                this.colorVectors[count] = 0.0;
+                this.directionVectors[count] = 0.0;
                 this.arrowPositions[ count++ ] = y;
 
-                this.slopeVectors[count] = Math.random();
+                this.colorVectors[count] = 0.0;
+                this.directionVectors[count] = 0.0;
                 this.arrowPositions[ count++ ] = 0;
             }
         }
 
         this.arrowInstance.addAttribute( 'offset', new THREE.InstancedBufferAttribute( this.arrowPositions, 3 ) );
-        this.arrowInstance.addAttribute( 'direction', new THREE.InstancedBufferAttribute( this.slopeVectors, 3 ) );
+        this.arrowInstance.addAttribute( 'direction', new THREE.InstancedBufferAttribute( this.directionVectors, 3 ) );
+        this.arrowInstance.addAttribute( 'color', new THREE.InstancedBufferAttribute( this.colorVectors, 3 ) );
+
+        // this.arrowInstance.addAttribute( 'angle' , new THREE.InstancedBufferAttribute( this.thetaVector, 1 ) );
+        
 
         var instancedMaterial = new THREE.RawShaderMaterial( {
+            uniforms : { 
+                         shouldNormalize : { value : true },
+                         scale : { value : 0.3 },
+                         useColor : { value : true },
+                         arrowAlpha : { value : this.arrowAlpha },
+                       },
             vertexShader: ARROW_VERTEX_SHADER,
             fragmentShader: ARROW_FRAGMENT_SHADER,
+            side : THREE.DoubleSide,
+            transparent : true,
         });
 
 
         this.arrowInstanceMesh = new THREE.Mesh( this.arrowInstance, instancedMaterial );
+
         // this.arrowInstanceMesh.scale.set( 1, 0.1, 0.1 );
         this.addMesh( scene, this.arrowInstanceMesh );
-        var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-        var mesh = new THREE.Mesh( arrowGeom, material );
+    };
 
-        this.addMesh( scene, mesh );
-        this.lineMeshes = [];
+    getArrowGeometry( headwidthratio=4.0, headwidth=0.25, headlength=0.4, n = 10 ){
 
-    }
+        var bodyToHeadRatio = headwidth / ( 2.0 * headwidthratio );
 
-    animate( scene, camera, timeStep ){
-        if ( this.modelShouldUpdate ){
-            this.udpateModel( scene );
-            this.modelShouldUpdate = false;
-        }
-    }
+        var vertices = [  0.0,               bodyToHeadRatio,  0.0,
+                          0.0,              -bodyToHeadRatio,  0.0,
+                          1.0 - headlength, -bodyToHeadRatio,  0.0,
 
-    getArrowGeometry( headpercent=0.1, n = 10 ){
+                          0.0,               bodyToHeadRatio, 0.0,
+                          1.0 - headlength, -bodyToHeadRatio, 0.0,
+                          1.0 - headlength,  bodyToHeadRatio, 0.0,
 
-        var vertices = [ 0.0, 0.0, 0.0,
-                     -2.0, 1.0, 0.0,
-                     -2.0, -1.0, 0.0 ];
+                          1.0 - headlength, -headwidth / 2.0, 0.0,
+                          1.0, 0.0, 0.0,
+                          1.0 - headlength, headwidth / 2.0, 0.0
+                          ];
 
         var vertsArray = new Float32Array( vertices );
         var bufferGeometry = new THREE.BufferGeometry();
@@ -237,7 +321,7 @@ class ODEDemo extends SuperModel{
         return bufferGeometry;
     }
         
-    udpateModel( scene ){
+    updateModel( scene, both = false ){
         
         // draw real function
         for( var i = 0; i < this.sceneMeshes.length; i++ ){
@@ -248,6 +332,23 @@ class ODEDemo extends SuperModel{
 
         var index = 0;
         var count = 0;
+        var ys, ts;
+        [ ys, ts ] = ODESolvers.euler( this.code, this.tStart, this.tEnd, this.h, this.y0 );
+
+        for ( var i = 0; i < ts.length; i++ ){
+            this.linePointsArray[ i * 3 + 0 ] = ts[i];
+            this.linePointsArray[ i * 3 + 1 ] = ys[i];
+            this.linePointsArray[ i * 3 + 2 ] = 0.0;
+        }
+
+        for ( var i = ts.length; i < this.maxLinePoints; i++ ){
+            this.linePointsArray[ i * 3 + 0 ] = ts[ts.length - 1];
+            this.linePointsArray[ i * 3 + 1 ] = ys[ys.length - 1];
+            this.linePointsArray[ i * 3 + 2 ] = 0;
+        }
+
+        this.lineMesh.geometry.attributes.position.needsUpdate = true;
+
         for ( var t = -5; t <= 5; t += 0.5 ){
             var yStart = -5
             var yEnd = 5
@@ -256,46 +357,34 @@ class ODEDemo extends SuperModel{
 
                 var sample = this.code.eval( { t : t, y : y } );
                 // var sample = currentFunction( t, y );
-                var dir = new THREE.Vector3( 1, sample, 0 );
-                var length = dir.length();
-                slopeLengths.push( length );
+                if ( both ){
+                    var dir = new THREE.Vector3( 1, sample, 0 );
+                    slopeLengths.push( dir.length() );
 
-                var otherDir = new THREE.Vector3( sample, 1, 0 );
-                this.arrowInstance.attributes.direction[ count++ ] = dir.x;
-                this.arrowInstance.attributes.direction[ count++ ] = dir.y;
-                this.arrowInstance.attributes.direction[ count++ ] = dir.z;
-
-                dir.normalize();
-
-                this.arrowMeshes[index].setDirection( dir );
-                if ( this.normalizeSlopes ){
-                    this.arrowMeshes[index].setLength( 0.3, 0.2, 0.1 );
-                } else {
-                    var arrSize = 0.5
-                    this.arrowMeshes[index].setLength( length * arrSize,
-                                                       Math.min( length * 0.2, 0.2 ),
-                                                       Math.min( length * 0.1, 0.1 ) );
+                    this.directionVectors[ count++ ] = dir.x;
+                    this.directionVectors[ count++ ] = dir.y;
+                    this.directionVectors[ count++ ] = dir.z;
                 }
-                index += 1;
             }
         }
 
-        this.arrowInstance.attributes.direction.needsUpdate = true;
+        if ( both ){
+            this.arrowInstanceMesh.geometry.attributes.direction.needsUpdate = true;
 
-        var maxLength = Math.max( ...slopeLengths );
-        maxLength += maxLength * 0.01;
+            var minSlope = Math.min( ...slopeLengths );
+            var maxSlope = Math.max( ...slopeLengths );
 
-        var maxColor = new THREE.Color( 0xaa0000 );
+            for ( var i = 0; i < slopeLengths.length; i++ ){
+                var alpha = ( slopeLengths[i] - minSlope ) / ( maxSlope - minSlope );
+                var arrowColor = new THREE.Color( this.minColor );
+                arrowColor.lerpHSL( new THREE.Color( this.maxColor ), alpha );
 
-        for ( var i = 0; i < this.arrowMeshes.length; i++ ){
-            if ( this.colorSlopes ){
-                var arrowColor = new THREE.Color( 0x0000aa );
-                arrowColor.lerpHSL( maxColor, slopeLengths[i] / maxLength );
-
-                this.arrowMeshes[i].setColor( arrowColor );
-            } else {
-                this.arrowMeshes[i].setColor( new THREE.Color( 0x555555 ) );
+                this.colorVectors[ 3 * i ] = arrowColor.r;
+                this.colorVectors[ 3 * i + 1 ] = arrowColor.g;
+                this.colorVectors[ 3 * i + 2 ] = arrowColor.b;
             }
+
+            this.arrowInstanceMesh.geometry.attributes.color.needsUpdate = true;
         }
     }
 
